26562d76282768a7d5a2e5b88b218e76c679d28e
Exosfeer <andrewnoorden@hotmail.com>
2021-06-09 21:14:11 +0200

    :lipstick: Home Page new banners and footer height improvements
    :lipstick: Home Page new banners and footer height improvements

--- trivia-app/src/Pages/Home.jsx
+++ trivia-app/src/Pages/Home.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import bannerSinglePlayer from '../banner_darkmode_single.png';
+import bannerMultiPlayer from '../banner_darkmode_multi.png';
+import bannerHostPlayer from '../banner_darkmode_host.png';
 
 function Home() {
   return (
     <div className="home">
       <div className="container">
-        <div className="row align-items-center my-5">
+        <div className="row align-items-center text-center my-5">
           <div className="col-lg-7">
               <h1> Game modes </h1>
-              <h2>Single Player</h2>
+            <div className="row justify-content-center">
+              <h5>Get to know the game by honing your skills within 'Single Player'</h5>
               <Link className="nav-link" to="/single-player">
-                  <img
-                    className="img-fluid rounded mb-4 mb-lg-0"
-                    src="https://plchldr.co/i/900x200?&bg=d37e00&fc=000&text=image_900x200"
-                    alt=""
-                    />
-                </Link>
-                <h2>Join a Lobby (PvP)</h2>
+                <img
+                  className="img-fluid rounded mb-4 mb-lg-0"
+                  src={bannerSinglePlayer}
+                  alt="bannerSinglePlayer"
+                  />
+              </Link>
+            </div>
+            <div className="row justify-content-center">
+                <h5>Join a Lobby (PvP)</h5>
                 <Link className="nav-link" to="/lobbies">
                     <img
                     className="img-fluid rounded mb-4 mb-lg-0"
-                    src="https://plchldr.co/i/900x200?&bg=d37e00&fc=000&text=image_900x200"
-                    alt=""
+                    src={bannerMultiPlayer}
+                    alt="bannerMultiPlayer"
                     />
                 </Link>
-                <h2>Host a Lobby</h2>
-                <Link className="nav-link" to="/lobbies">
+                </div>
+            <div className="row justify-content-center">
+                <h5>Host a Lobby</h5>
+                <Link className="nav-link" to="/lobby-host">
                     <img
                     className="img-fluid rounded mb-4 mb-lg-0"
-                    src="https://plchldr.co/i/900x200?&bg=d37e00&fc=000&text=image_900x200"
-                    alt=""
+                    src={bannerHostPlayer}
+                    alt="bannerHostPlayer"
                     />
                 </Link>
+                </div>
                 </div>
           <div className="col-lg-5 width-70">
             <h1 className="font-weight-light">What is Treevia?</h1>
